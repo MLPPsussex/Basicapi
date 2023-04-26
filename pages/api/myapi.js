6b1042fcc2d652b9e9b7c2b1f98e71ba437c7411
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-04-26 12:35:05 +0100

    Update myapi.js

--- pages/api/myapi.js
+++ pages/api/myapi.js
@@ -1,12 +1,3 @@
 export default function handler(req, res) {
-  const requestMethod = req.method;
-  const body = JSON.parse(req.body);
-  switch (requestMethod) {
-    case 'POST':
-      res.status(200).json({ message: `You submitted the following data: ${body}` })
- 
-    // handle other HTTP methods
-    default:
-      res.status(200).json({ message: 'Welcome to myAPI!'})
-  }
+      res.status(200).json({ message: `You submitted Hello World` })
 }
