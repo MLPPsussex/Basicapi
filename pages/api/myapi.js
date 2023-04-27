9a32bd9ec3825f2c7d50037a415e112454c86f02
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-04-27 05:21:28 +0100

    Update myapi.js

--- pages/api/myapi.js
+++ pages/api/myapi.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
  // const requestMethod = req.method;
-  const body = JSON.parse(req.body);
+  const body = req.body
 //  switch (requestMethod) {
 //   case 'POST':
       res.status(200).json({ message: `You submitted: ${body}` })
