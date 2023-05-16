88d7ba5201b2e3d5cdab847c16e4f7f652c6e927
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-17 00:45:32 +0100

    Update myapi.js

--- pages/api/myapi.js
+++ pages/api/myapi.js
@@ -1,16 +1,16 @@
 export default function handler(req, res) {
- var fs = require('fs');
+// var fs = require('fs');
  // const requestMethod = req.method;
   const body = req.body
 //  switch (requestMethod) {
 //   case 'POST':
   
-  const data = fs.readFileSync("mydata.txt")
+  //const data = fs.readFileSync("mydata.txt")
   if(body=="JEREMY"){
       res.status(200).json({ message: `You are logged in ${body}` })
   }
   else{
-   res.status(200).json({ message: data })
+   res.status(200).json({ message: 'fred' })
   }
    
 //  default:
