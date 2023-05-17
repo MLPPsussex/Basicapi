73c5494e9a2263db486477a2eca3e4902398c9a1
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-17 02:02:58 +0100

    Update myapi.js

--- app/api/myapi.js
+++ app/api/myapi.js
@@ -1,8 +1,6 @@
 export default function handler(req, res) {
   var myfile = require('fs');
   var msg = myfile.readFileSync("mydata.txt")
-  var l=msg.length
   if(req.body=="Me"){ res.status(200).json({ message: `Logged in` }) }
-  else{ res.status(200).json({ message: `Error ${l}` })  }
-   
+  else{ res.status(200).json({ message: `Error ${msg}` })  } 
 }
