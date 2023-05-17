e521d1b14df0bea9e12bf3b0125b57e57673f220
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-17 10:06:56 +0100

    Update route.js

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -4,6 +4,12 @@ var fs = require('fs');
 export async function GET() {
   //const data="This is a test"
  
+   fs.writeFileSync("mydata.txt",
+    "Movie ",
+    { flag: "a+",}
+                   );
+
+ 
   var msg = fs.readFileSync("mydata.txt")
   msg+="**"
  
