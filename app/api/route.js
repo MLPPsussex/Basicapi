30e34e09ef2a2b974d650607457a221f8662d1fd
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-17 08:20:32 +0100

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
  
-export async function GET() {
+export async function POST() {
   const data="This is a test"
  
   return NextResponse.json({ data });
