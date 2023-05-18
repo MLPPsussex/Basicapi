8f653070bc62ab184b42029fd4aef3eb98410ada
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-18 21:14:52 +0100

    Update route.js

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -2,16 +2,7 @@ import { NextResponse } from 'next/server';
 var fs = require('fs');
  
 export async function GET() {
-  //const data="This is a test"
- 
-   fs.writeFileSync("mydata.txt",
-    "Movie ",
-    { flag: "a+",}
-                   );
-
- 
   var msg = fs.readFileSync("mydata.txt")
-  msg+="**"
  
   return NextResponse.json({ message : msg });
 }
