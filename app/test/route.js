4f8ace3bfd6b477bb62d569160a483698dcad3ce
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-19 07:55:51 +0200

    Create route.js

---
+++ app/test/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+var fs = require('fs');
+ 
+export async function GET() {
+  var msg = ""
+  msg=fs.readFileSync("mydata.txt")
+  msg+="^*"
+ 
+  return NextResponse.json({ message: msg , color: 'blue'});
+}
