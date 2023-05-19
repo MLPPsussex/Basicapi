a8f60fc5fa8b439655df2fb4f83b6a5be80ad4c1
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-19 08:49:39 +0200

    Update route.js

--- app/locate/route.js
+++ app/locate/route.js
@@ -1,7 +1,5 @@
 import { kv } from "@vercel/kv";
 
 export default async function loc() {
-  const num //= await kv.get<{ id: string; quantity: number }[]>(params.user);
-  return (
-  );
+  await kv.incr('views')
 }
