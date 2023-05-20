f79f252ce344b00a3cafa9bac879fa8b38554955
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-20 10:37:11 +0200

    Update route.js

--- app/locate/route.js
+++ app/locate/route.js
@@ -1,7 +1,7 @@
 import { kv } from "@vercel/kv";
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export default async function GET() {
   var num=await kv.incr('views')
   return NextResponse.json({ number: num});
 }
